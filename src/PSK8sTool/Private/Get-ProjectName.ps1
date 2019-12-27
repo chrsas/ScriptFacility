eb43d595575a65c77b635a084e5cf85c26810800
chrsas <chrsas@outlook.com>
2019-12-27 11:09:41 +0800

    增加Readme单行信息的处理

--- src/PSK8sTool/Private/Get-ProjectName.ps1
+++ src/PSK8sTool/Private/Get-ProjectName.ps1
@@ -1,5 +1,11 @@
 function Get-ProjectName {
     $readme = Get-Content .\README.md;
-    $projectName = $readme[0].Substring(2).Replace("/", "\");
-    return $projectName;
+    # 单行的README, 读出来的是 string
+    if ($readme.GetType().FullName -eq "System.String") {
+        $firstLine = $readme;
+    }
+    else {
+        $firstLine = $readme[0];
+    }
+    return $firstLine.Replace("# ", "").Replace("/", "\");
 }

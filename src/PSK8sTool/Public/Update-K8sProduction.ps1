3d8082c3ab890b8da7673de150bf12cb2f316903
chrsas <chrsas@outlook.com>
2019-12-27 17:05:57 +0800

    重构 增加对多个image和非标准image的处理

--- src/PSK8sTool/Public/Update-K8sProduction.ps1
+++ src/PSK8sTool/Public/Update-K8sProduction.ps1
@@ -2,55 +2,97 @@ function Update-K8sProduction {
     param ( )
     Import-Module powershell-yaml;
     $pPathList = Get-ChildItem -Directory -Exclude web-ui, notification;
-    "下列服务将升级, 请仔细核对:"
-    $upServices = New-Object 'Collections.Generic.List[string]'
-    $upServices.Add("# $(Get-Date -Format yyyy-MM-dd)")
-    $newGroup = [String]::Empty;
+    $upServices = New-Object 'Collections.Generic.List[UpgradeInfo]'
     $hasOverlays = $false;
     foreach ($pPath in $pPathList) {
         # 找到 overlays 目录下的 staging 和 production 的yaml
-        Get-ChildItem $pPath -Depth 1 -Directory overlays | ForEach-Object {
+        foreach ($overlay in Get-ChildItem $pPath -Depth 2 -Directory overlays) {
             $hasOverlays = $true;
-            if(!(Test-Path $_/staging/kustomization.yaml)){
+            if (!(Test-Path $overlay/staging/kustomization.yaml)) {
                 continue;
             }
-            $productionSource = Get-Content $_/production/kustomization.yaml;
-            $production = ConvertFrom-Yaml $(Get-Content -Raw $_/production/kustomization.yaml);
-            $staging = ConvertFrom-Yaml $(Get-Content -Raw $_/staging/kustomization.yaml);
-            if([string]::IsNullOrWhiteSpace($staging.images.newTag)){
+            $productionSource = Get-Content $overlay/production/kustomization.yaml;
+            $production = ConvertFrom-Yaml $(Get-Content -Raw $overlay/production/kustomization.yaml);
+            $staging = ConvertFrom-Yaml $(Get-Content -Raw $overlay/staging/kustomization.yaml);
+            if ([string]::IsNullOrWhiteSpace($staging.images.newTag)) {
                 # 依赖的基础服务没有newTag
                 continue;
             }
-            if([String]::IsNullOrWhiteSpace($production.commonLabels.app)){
-                Write-Host "$_\production\kustomization.yaml 没有 commonLabels.app" -ForegroundColor DarkMagenta
+            if ([String]::IsNullOrWhiteSpace($production.commonLabels.app)) {
+                Write-Host "$overlay\production\kustomization.yaml 没有 commonLabels.app" -ForegroundColor DarkMagenta
                 continue;
             }
-            if ($production.images.newTag -ne $staging.images.newTag) {
-                $group = $production.commonLabels.app.Split('-')[0];
-                # 服务组变换, 增加横线
-                if (($newGroup -ne $group) -and ($newGroup.Length -ne 0) ) {
-                    $upServices.Add("---");
+            if ($production.images.newTag.GetType().FullName -eq "System.Object[]") {
+                # 针对多个镜像的情况
+                if (!([System.Linq.Enumerable]::SequenceEqual([object[]]$production.images.newTag, [object[]]$staging.images.newTag))) {
+                    $info = [UpgradeInfo]::new();
+                    $info.App = $production.commonLabels.app;
+                    $info.Message = "有多个image, 需要手动处理, PS: 自动处理的代码需要你的帮助 :-)";
+                    $upServices.Add($info);
                 }
-                $newGroup = $group;
-                $upServices.Add("- [ ] $($production.commonLabels.app) $($staging.images.newTag)")
-                Write-Host $production.commonLabels.app $production.images.newTag =>
-                Write-host $staging.images.newTag -ForegroundColor DarkCyan
-                $tagStr = $productionSource[$productionSource.Length - 1];
-                # 转换后的Yaml对象保存后, 其格式很怪异, 所以这里直接用文本转换
-                $productionSource[$productionSource.Length - 1] = "$($tagStr.Split(":")[0]): $($staging.images.newTag)"
-                $productionSource > $_/production/kustomization.yaml
+                continue;
             }
+            if ($production.images.newTag -eq $staging.images.newTag) {
+                continue;
+            }
+            $info = [UpgradeInfo]::new();
+            $info.App = $production.commonLabels.app;
+            $info.NewTag = $staging.images.newTag;
+            $info.OldTag = $production.images.newTag;
+            $upServices.Add($info);
+            $tagStr = $productionSource[$productionSource.Length - 1];
+            # 转换后的Yaml对象保存后, 其格式很怪异, 所以这里直接用文本转换
+            $productionSource[$productionSource.Length - 1] = "$($tagStr.Split(":")[0]): $($staging.images.newTag)"
+            $productionSource > $overlay/production/kustomization.yaml
         }
     }
     if (!($hasOverlays)) {
         Write-Host "当前目录的子目录里没有 overlays" -ForegroundColor Red
         return;
     }
-    if ($upServices.Count -eq 1) {
+    if ($upServices.Count -eq 0) {
         #有一行日期信息在里面
         Write-Host "没有需要升级的服务" -ForegroundColor Red
         return;
     }
+    Write-Host "下列服务将升级, 请仔细核对:" -ForegroundColor DarkYellow
+    # 着色方法 https://stackoverflow.com/a/49038815/4052810
+    $upServices | Format-Table App, OldTag, @{
+        Label      = "NewTag"
+        Expression = {
+            $color = "32"
+            $e = [char]27
+            "$e[${color}m$($_.NewTag)${e}[0m"
+        }
+    },@{
+        Label = "Message"
+        Expression = {
+            $color = "31"
+            $e = [char]27
+            "$e[${color}m$($_.Message)${e}[0m"
+        }
+    }
+    $strList = New-Object "System.Collections.Generic.List[String]";
+    $oldGroup = $upServices[0].App.Split('-')[0];
+    foreach ($info in $upServices) {
+        $newGroup = $info.App.Split('-')[0];
+        # 增加分组行
+        if ($oldGroup -ne $newGroup) {
+            $oldGroup = $newGroup;
+            $strList.Add("---");
+        }
+        $strList.Add("- [ ] $($info.App) $($info.NewTag)");
+    }
+    $strList | clip
     Write-Host "发布内容已经复制到粘贴板" -ForegroundColor DarkCyan
-    $upServices | clip
 }
+
+class UpgradeInfo {
+    [string]$App;
+    [string]$OldTag;
+    [string]$NewTag;
+    [string]$Message;
+}
+
+cd 'C:\GitLab\Exceed\Kubernetes\Deployment'
+Update-K8sProduction

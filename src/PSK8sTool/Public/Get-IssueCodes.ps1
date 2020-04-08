874ec0e24acdd1e504f58b2145c1b67263c65c48
chrsas <chrsas@outlook.com>
2020-04-08 21:52:23 +0800

    解决获取issue失败的问题

--- src/PSK8sTool/Public/Get-IssueCodes.ps1
+++ src/PSK8sTool/Public/Get-IssueCodes.ps1
@@ -4,10 +4,10 @@ function Get-IssueCodes {
         [string] $tag
     )
     if ([String]::IsNullOrWhiteSpace($tag)) {
-        $tag = git describe --abbrev=0
+        $tag = git describe --tags --abbrev=0
     }
     $set = New-Object System.Collections.Generic.HashSet[string];
-    git log 1.2.10..HEAD --oneline |
+    Invoke-Expression "git log $tag..HEAD --oneline" |
     ForEach-Object {
         if ($_ -match '#\d+') {
             foreach ($ma in $Matches) {
